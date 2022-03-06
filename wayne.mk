61262229d52342cea8743432262dda2cd8c629ba
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-03-06 21:49:01 +0330

    wayne-common: Remove LineageOS stuffs
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I74821063125a9c948c15be440aa6a2c4fd386905

--- wayne.mk
+++ wayne.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2022 AospExtended ROM Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,7 +14,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-extended
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -21,10 +22,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2160
-TARGET_SCREEN_WIDTH := 1080
 
 # Consumerir
 BOARD_HAVE_IR := true
